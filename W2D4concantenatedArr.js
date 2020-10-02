9e4d8c47b424ba4712dac09f3d2dbafc4b63bb87
luwites <luwamtes111@gmail.com>
2020-10-01 21:16:48 -0500

    W2D4

---
+++ W2D4concantenatedArr.js
@@ -0,0 +1,8 @@
+let helen, thomas, mary, greg;
+helen = ["bed", true, "chair"];
+thomas = ["tall", 33];
+mary = "pretty";
+greg = [35, "car"]
+copyOfA = [...helen]
+concantenatedArr = helen.concat(mary, thomas, greg); 
+console.log(concantenatedArr);
---
+++ W2D4copyOfArr.js
@@ -0,0 +1,9 @@
+let helen, thomas, mary, greg;
+helen = ["bed", true, "chair"];
+thomas = ["tall", 33];
+mary = "pretty";
+greg = [35, "car"]
+copyOfHelen = [...helen];
+copyOfThomas = [...thomas];
+console.log(copyOfHelen);
+console.log(copyOfThomas);
