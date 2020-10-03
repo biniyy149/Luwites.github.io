3ee9d1839838a576d84ac40b1c46f38a6852c154
luwites <luwamtes111@gmail.com>
2020-10-03 01:41:43 -0500

    Completed

--- W2D4checkForTheTarget.js
+++ W2D4checkForTheTarget.js
@@ -1,12 +1,40 @@
-class Node {
-    constructor(value, target) {
+class TreeNode {
+    constructor(value) {
         this.value = value;
-        this.next = next;
+        this.descendents = [];
     }
 }
-// create nodes with values
-const abe = new Node('Abe');
-const homer = new Node('Homer');
-const bart = new Node('Bart');
-const lisa = new Node('Lisa');
-const maggie = new Node('Maggie');
+
+const abe = new TreeNode('Abe');
+const homer = new TreeNode('Homer');
+const bart = new TreeNode('Bart');
+const lisa = new TreeNode('Lisa');
+const maggie = new TreeNode('Maggie');
+
+abe.descendents.push(homer);
+homer.descendents.push(bart, lisa, maggie);
+
+function contains(node, target) {
+    
+    if (node.descendents === null) {
+        if (node.value === target) {
+            return true;
+        } else {
+            return false;
+        }
+    } else { 
+        let childResult = false;
+        if (node.value === target) {
+            return true;
+        }
+        for (let child of node.descendents) {
+            childResult = contains(child, target);
+            if (childResult === true) {
+                return true;
+            }
+        }
+        return false; 
+    }
+}
+console.log(contains(homer, "Abe"));
+console.log(contains(homer, "Maggie")); 
