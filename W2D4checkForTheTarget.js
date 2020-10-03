896e6a475e6a019062118312ab4db74424aa1cb6
luwites <luwamtes111@gmail.com>
2020-10-02 21:31:49 -0500

    nf

---
+++ W2D4checkForTheTarget.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor(value, target) {
+        this.value = value;
+        this.next = next;
+    }
+}
+// create nodes with values
+const abe = new Node('Abe');
+const homer = new Node('Homer');
+const bart = new Node('Bart');
+const lisa = new Node('Lisa');
+const maggie = new Node('Maggie');
--- W2D4concantenatedArr.js
+++ W2D4concantenatedArr.js
@@ -3,6 +3,5 @@ helen = ["bed", true, "chair"];
 thomas = ["tall", 33];
 mary = "pretty";
 greg = [35, "car"]
-copyOfA = [...helen]
 concantenatedArr = helen.concat(mary, thomas, greg); 
 console.log(concantenatedArr);
