1209a7f68d23fb3972dd1fc2b4decdfb6a06a289
luwites <luwamtes111@gmail.com>
2020-10-03 22:17:37 -0500

    nf

---
+++ W2D5isVariableVisible.js
@@ -0,0 +1,9 @@
+let x = 1;
+
+function func() {
+  console.log(x); 
+  let x = 2;
+}
+
+func(); // At first I thought there will be error because we are trying to declare a variable again down in the function
+        // but now i can see that the error is because of access before initialization
