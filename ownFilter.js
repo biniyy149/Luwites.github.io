b3822a75f1dc4835f4ee73aad4d765c2f55ffb4e
luwites <luwamtes111@gmail.com>
2020-10-08 23:19:59 -0500

    Create ownFilter.js

---
+++ ownFilter.js
@@ -0,0 +1,9 @@
+let users = [ {id: 1, name: "John"}, {id: 2, name: "Pete"}, {id: 3, name: "Mary"} ];
+function pri(arr) {
+    for(let  of arr) {
+        if(users.id > 2) {
+            return users.name
+        }
+    }
+}
+pri();
