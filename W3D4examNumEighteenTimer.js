785cf97fb4ecd16851496f5bd84b6c3a65a2ce6a
luwites <luwamtes111@gmail.com>
2020-10-09 21:30:05 -0500

    nf

---
+++ W3D4examNumEighteenTimer.js
@@ -0,0 +1,15 @@
+function tenClock() {
+    let second = 0;
+    let timer = setInterval(function () {
+        let date = new Date();
+        console.log(
+            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+        );
+        second++;
+        if (second === 10) {
+            clearTimeout(timer);
+        }
+    }, 1000);
+}
+
+tenClock();
