39252bcb9848e3c83aec8b097bd3e4c610088e63
luwites <luwamtes111@gmail.com>
2020-10-22 11:50:39 -0500

    nf

---
+++ project/actionFinal.js
@@ -0,0 +1,129 @@
+/*eslint-disable*/
+
+function returnBook() {
+    let student = library.students.get(document.getElementById("student_name").value);
+    let book = library.librarybooks.get(document.getElementById("book_title").value);
+    student.returnBook(book);
+    displayInfo();
+}
+
+// function returnAllBooks() {
+//     let student = library.students.get(document.getElementById("student_name").value);
+//     let books = currentStudent.libraryCard.booksOut.map(obj => obj.name);
+//     document.getElementById("info").innerHTML = `Hi ${currentStudent.name}, your current books are: ${books}`;
+
+// }
+// function total(book) {
+//     let owed = book.overDueFee();
+//     document.getElementById("total").innerHTML = `Hi ${currentStudent.name}, your total: ${owed}`;
+// }
+
+function add(event) {
+    let book = library.librarybooks.get(event.target.parentNode.value);
+    checkOut(book);
+
+}
+
+function updateBooksOut(book) {
+    let table = document.getElementById("booksOut");
+    let row = document.createElement("tr");
+    let td1 = document.createElement("td");
+    td1.innerHTML = book.name;
+    let td2 = document.createElement("td");
+    td2.innerHTML = book.overDueFee();
+    let td3 = document.createElement("td");
+    td3.innerHTML = book.dueDate;
+    row.append(td1);
+    row.append(td2);
+    row.append(td3);
+    table.append(row);
+}
+
+let currentStudent;
+
+function pullInfo() {
+    let student = library.students.get(document.getElementById("student_name").value);
+    if (!student) {
+        alert("Student Name Not Found, Please Try Again.");
+        return;
+    }
+    currentStudent = student;
+    displayInfo();
+}
+
+function displayInfo() {
+    clearAllBooks();
+    let books = currentStudent.libraryCard.booksOut.map(obj => obj.name);
+    document.getElementById("info").innerHTML = `Hi ${currentStudent.name}, click on the books you would like to borrow: ${books}`;
+    if (currentStudent.libraryCard.booksOut.length > 0) {
+        for (let book of currentStudent.libraryCard.booksOut) {
+            let table = document.getElementById("booksOut");
+            let row = document.createElement("tr");
+            let td1 = document.createElement("td");
+            td1.innerHTML = book.name;
+            let td2 = document.createElement("td");
+            td2.innerHTML = book.overDueFee();
+            let td3 = document.createElement("td");
+            td3.innerHTML = book.dueDate;
+            row.append(td1);
+            row.append(td2);
+            row.append(td3);
+            table.append(row);
+        }
+    }
+}
+
+function checkOut(book) {
+    if (!currentStudent) {
+        alert("Please enter your name and then select books to borrow.")
+        return;
+    }
+    let books_before_checkout = currentStudent.libraryCard.booksOut;
+    try {
+        currentStudent.checkOutBook(book);
+    } catch (e) {
+        if (e instanceof ApplicationError) {
+            alert("error!");
+            currentStudent.libraryCard.booksOut = books_before_checkout;
+            return;
+        }
+    }
+    let books = currentStudent.libraryCard.booksOut.map(obj => obj.name);
+    document.getElementById("info").innerHTML = (`Thanks for visiting, your books are ${books}`);
+    updateBooksOut(book)
+}
+
+function clearAllBooks() {
+    let table = document.getElementById('booksOut');
+    let rows = document.querySelectorAll('#booksOut>tr')
+    for (let i = rows.length; i > 0; i--) {
+        table.deleteRow(i);
+    }
+
+}
+
+function loginTest() {
+
+    let userId = document.getElementById("login").value;
+    let passWord = document.getElementById("password").value;
+
+
+    if ("Luwam" === userId && "hello" === passWord || "Tahir" === userId && "hey" === passWord) {
+        window.location = "index.html";
+        return;
+    }
+    alert("Incorrect user Id or password, please sign up to with your Id and Password");
+}
+
+function register() {
+    alert("Redirecting to the registration page...")
+        window.location = "register.html"
+}
+function redirectToLibrary(){
+
+        window.location = "index.html"
+   
+}
+function finishBorrowing() {
+    clearAllBooks();
+}
---
+++ project/libraryFinal.js
@@ -0,0 +1,132 @@
+
+/*eslint-disable*/
+class ApplicationError extends Error{};
+
+class Student {
+    constructor(name, libraryCard) {
+        this._name = name;
+        this._libraryCard = libraryCard;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get libraryCard(){
+        return this._libraryCard;
+    }
+
+    set libraryCard(libraryCard){
+        this._libraryCard = libraryCard;
+    }
+
+    checkOutBook(book) {
+        let count = 0;
+        let now = new Date();
+        if(this.libraryCard.booksOut.length > 0){
+            for(let book of this.libraryCard.booksOut){
+                if(now !== book.dueDate){
+                    count ++;
+                }
+            }
+        } else {
+            this.libraryCard.booksOut.push(book);
+            book.dueDate = book.calcDueDate();
+        }
+        if(count){
+            this.libraryCard.booksOut.push(book);
+            book.dueDate = book.calcDueDate(); 
+        }
+    }
+    returnBook(book) {
+        for(let volume of this.libraryCard.booksOut){
+            if(volume.name === document.getElementById("book_title").value){
+                let index = this.libraryCard.booksOut.indexOf(book);
+                this.libraryCard.booksOut.splice(index,1); 
+            }
+        }
+    }
+}
+class Book {
+
+    constructor(name, author) {
+        this.name = name;
+        this.author = author;
+        this.calcDueDate = function(){
+            let numWeeks = 2;
+            let now = new Date();
+            now.setDate(now.getDate() + 7)
+            return now;
+        }
+        this._dueDate = 0;
+        this.overDueFee = function(){
+            let overDueRateFee = 0.50;
+            const date1 = new Date(this.dueDate);
+            const date2 = new Date();
+            let diffTime = date2 - date1;
+            if(diffTime <= 0){
+                diffTime = 0;
+            }
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+            return `$${overDueRateFee* diffDays}`
+        }
+    } set dueDate (date){
+        this._dueDate = date;
+    } get dueDate (){
+        return this._dueDate;
+    }
+}
+
+class LibraryCard {
+    constructor() {
+        this._booksOut = [];
+    }
+    set booksOut(book){
+        this._booksOut += book;
+    }
+    get booksOut(){
+        return this._booksOut;
+    }
+}
+
+
+class Library {
+    constructor(librarybooks, students) {
+        this.librarybooks = librarybooks;
+        this.students = students;
+    }
+
+    addNewBook(newBook) {
+        this.libraryBooks.set(newbook.name, newBook);
+    }
+
+    addStudent(newStudent) {
+        this.students.set(newStudent.name, newStudent);
+    }
+}
+
+
+const libraryBooks = new Map()
+
+libraryBooks.set("The Secret", new Book("The Secret", "Rhonde"));
+libraryBooks.set("The Sky Is Falling", new Book("The Sky Is Falling", "Sydney Sheldon"));
+libraryBooks.set("The Jewels", new Book("The Jewels", "Daniella Steel"));
+libraryBooks.set("Sleeping Murder", new Book("Sleeping Murder", "Agatha Christie"));
+libraryBooks.set("Where The Forest Meets The Stars", new Book("Where The Forest Meets The Stars", "Glendy Vanderah"));
+libraryBooks.set("Think Big Act Small", new Book("Think Big Act Small", "Jason Jennings"));
+
+const students = new Map();
+students.set("Tahir", new Student("Tahir", new LibraryCard()));
+students.set("Luwam", new Student("Luwam", new LibraryCard()))
+
+
+const library = new Library(libraryBooks, students);
+library.addStudent(new Student("Tahir", new LibraryCard()));
+library.addStudent(new Student("Luwam", new LibraryCard()))
+
+
+
